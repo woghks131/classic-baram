f248465033f9b0632accf919c656437f65dccbda
박재환 <woghks131@kakao.com>
2025-01-27 16:05:52 +0900

    mixins, store 기본 설정정

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+import mixins from './mixins'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).mixin(mixins).use(store).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const target = 'http://127.0.0.1:9091';
+module.exports = {
+  devServer: {
+    port: 9090,
+    proxy: {
+      '^/api': {
+        target,
+        changeOrigin: true
+      },
+      '^/upload': {
+        target,
+        changeOrigin: true
+      },
+      '^/download': {
+        target,
+        changeOrigin: true
+      },
+    }
+  }
+}
